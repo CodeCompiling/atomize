54386b60a9c5184857c46c1e4046be7923d9d57c
Christopher Wienberg <christopher.wienberg@gmail.com>
2014-02-07 10:25:34 -0800

    properly handles 2 and 3

--- setup.py
+++ setup.py
@@ -3,20 +3,34 @@
 from distutils.core import setup
 import sys
 
-sys.path = ["atomize"] + sys.path
+# handle python 2 vs 3
+if sys.version_info < (3,): # python 2
+    sys.path = ["src2"] + sys.path
+    package_dir = {"": "src2"}
+else:
+    sys.path = ["src3"] + sys.path
+    package_dir = {"": "src3"}
 import atomize
 
-setup(name="atomize",
-      version="%s.%s.%s" % atomize.__version__,
-      author="Christopher Wienberg",
-      author_email="cwienberg@ict.usc.edu",
-      url="http://code.google.com/p/atomize/",
-      download_url="http://code.google.com/p/atomize/downloads/list",
-      description="A simple Python package for easily generating Atom feeds",
-      long_description="A pure-Python package for easily generating Atom Syndicated Format feeds.",
-      package_dir={"": "atomize"},
-      py_modules=["atomize"],
-      provides=["atomize"],
-      keywords="atom web",
-      license="Eclipse Public License 1.0")
-      
+setup(
+    name="atomize",
+    version="%s.%s.%s" % atomize.__version__,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary'
+        ],
+    author="Christopher Wienberg",
+    author_email="cwienberg@ict.usc.edu",
+    url="http://code.google.com/p/atomize/",
+    download_url="http://code.google.com/p/atomize/downloads/list",
+    description=("A simple Python package for easily generating "
+                  "Atom feeds"),
+    long_description=("A pure-Python package for easily generating "
+                       "Atom Syndicated Format feeds."),
+    package_dir=package_dir,
+    py_modules=["atomize"],
+    provides=["atomize"],
+    keywords="atom feed web",
+    license="Eclipse Public License 1.0"
+)
